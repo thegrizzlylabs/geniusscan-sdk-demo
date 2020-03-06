10b79cd9f87110543f187ea13f1b87770aaca345
The Grizzly Labs <sdk@thegrizzlylabs.com>
2020-03-06 08:20:32 +0000

    Update demos for version 3.0.11

--- flutter-plugin-genius-scan-demo/android/app/src/main/java/com/thegrizzlylabs/geniusscan/sdk/flutter_genius_scan_example/MainActivity.java
+++ flutter-plugin-genius-scan-demo/android/app/src/main/java/com/thegrizzlylabs/geniusscan/sdk/flutter_genius_scan_example/MainActivity.java
@@ -1,13 +1,14 @@
 package com.thegrizzlylabs.geniusscan.sdk.flutter_genius_scan_example;
 
-import android.os.Bundle;
-import io.flutter.app.FlutterActivity;
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity;
+import io.flutter.embedding.engine.FlutterEngine;
 import io.flutter.plugins.GeneratedPluginRegistrant;
 
 public class MainActivity extends FlutterActivity {
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    GeneratedPluginRegistrant.registerWith(this);
+
+  public void configureFlutterEngine(@NonNull FlutterEngine flutterEngine) {
+    GeneratedPluginRegistrant.registerWith(flutterEngine);
   }
+
 }
