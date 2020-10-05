4354fbf7cb88e105d2dca7407727f2349aa3327e
The Grizzly Labs <sdk@thegrizzlylabs.com>
2020-10-05 07:27:01 +0000

    Update demos for version 4.0.0-beta2

---
+++ android/demo-custom/src/main/java/com/geniusscansdk/demo/camera/ScanActivity.java
@@ -0,0 +1,213 @@
+package com.geniusscansdk.demo.camera;
+
+import android.Manifest;
+import android.app.AlertDialog;
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.hardware.Camera;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.geniusscansdk.camera.CameraManager;
+import com.geniusscansdk.camera.FocusIndicator;
+import com.geniusscansdk.camera.ScanFragment;
+import com.geniusscansdk.camera.realtime.BorderDetector;
+import com.geniusscansdk.core.GeniusScanSDK;
+import com.geniusscansdk.core.LicenseException;
+import com.geniusscansdk.core.QuadStreamAnalyzer;
+import com.geniusscansdk.core.RotationAngle;
+import com.geniusscansdk.demo.R;
+import com.geniusscansdk.demo.model.Page;
+import com.geniusscansdk.demo.processing.BorderDetectionActivity;
+
+import java.io.File;
+
+public class ScanActivity extends AppCompatActivity implements ScanFragment.CameraCallbackProvider {
+   private static final String TAG = ScanActivity.class.getSimpleName();
+   private static final int PERMISSION_REQUEST_CODE = 1;
+
+   private ScanFragment scanFragment;
+   private TextView userGuidanceTextView;
+   private Page page;
+
+   private boolean cameraPermissionGranted = false;
+
+   @Override
+   public void onCreate(Bundle savedInstanceState) {
+      super.onCreate(savedInstanceState);
+      // Go full screen
+      getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+      setContentView(R.layout.scanning_activity);
+
+      Button captureButton = findViewById(R.id.captureButton);
+      captureButton.setOnClickListener(new View.OnClickListener() {
+         @Override
+         public void onClick(View v) {
+            takePicture();
+         }
+      });
+
+      userGuidanceTextView = findViewById(R.id.user_guidance);
+
+      FocusIndicator focusIndicator = findViewById(R.id.focus_indicator);
+
+      scanFragment = (ScanFragment) getSupportFragmentManager().findFragmentById(R.id.scan_fragment);
+      scanFragment.setOverlayColorResource(R.color.blue);
+      scanFragment.setPreviewAspectFill(false);
+      scanFragment.setRealTimeDetectionEnabled(true);
+      scanFragment.setFocusIndicator(focusIndicator);
+      scanFragment.setAutoTriggerAnimationEnabled(true);
+      scanFragment.setBorderDetectorListener(new BorderDetector.BorderDetectorListener() {
+         @Override
+         public void onBorderDetectionResult(QuadStreamAnalyzer.Result result) {
+            if (result.status == QuadStreamAnalyzer.Status.TRIGGER) {
+               takePicture();
+            }
+            updateUserGuidance(result);
+         }
+
+         @Override
+         public void onBorderDetectionFailure(Exception e) {
+            scanFragment.setPreviewEnabled(false);
+            new AlertDialog.Builder(ScanActivity.this)
+                    .setMessage(e.getMessage())
+                    .setCancelable(false)
+                    .setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+                       @Override
+                       public void onClick(DialogInterface dialog, int which) {
+                          finish();
+                       }
+                    })
+                    .show();
+         }
+      });
+
+      page = new Page();
+
+      cameraPermissionGranted = ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED;
+      if (!cameraPermissionGranted) {
+         ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA}, PERMISSION_REQUEST_CODE);
+      }
+   }
+
+   @Override
+   protected void onResume() {
+      super.onResume();
+
+      if (cameraPermissionGranted) {
+         scanFragment.initializeCamera();
+      }
+   }
+
+   @Override
+   public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+      cameraPermissionGranted = requestCode == PERMISSION_REQUEST_CODE && grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED;
+      // Camera will be initialized in onResume
+   }
+
+   private void takePicture() {
+      scanFragment.takePicture(new File(page.getOriginalImage().getAbsolutePath(this)));
+   }
+
+   private void updateUserGuidance(QuadStreamAnalyzer.Result result) {
+      int textResId = getUserGuidanceResId(result);
+      if (textResId == 0) {
+         userGuidanceTextView.setVisibility(View.INVISIBLE);
+      } else {
+         userGuidanceTextView.setVisibility(View.VISIBLE);
+         userGuidanceTextView.setText(textResId);
+      }
+   }
+
+   private int getUserGuidanceResId(QuadStreamAnalyzer.Result result) {
+      if (result.status == QuadStreamAnalyzer.Status.NOT_FOUND || result.resultQuadrangle == null) {
+         return R.string.detection_status_searching;
+      } else if (result.status == QuadStreamAnalyzer.Status.SEARCHING || result.status == QuadStreamAnalyzer.Status.ABOUT_TO_TRIGGER) {
+         return R.string.detection_status_found;
+      }
+      return 0;
+   }
+
+   @Override
+   public CameraManager.Callback getCameraManagerCallback() {
+      return new CameraManager.Callback() {
+         @Override
+         public void onCameraReady(CameraManager cameraManager, Camera camera) {}
+
+         @Override
+         public void onCameraFailure(CameraManager cameraManager) {}
+
+         @Override
+         public void onShutterTriggered(CameraManager cameraManager) {}
+
+         @Override
+         public void onPictureTaken(CameraManager cameraManager, int cameraOrientation, File outputFile) {
+            new RotateTask(page, RotationAngle.fromDegrees(cameraOrientation)).execute();
+         }
+      };
+   }
+
+   class RotateTask extends AsyncTask<Void, Void, Void> {
+
+      private RotationAngle rotationAngle;
+      private Page page;
+      private ProgressDialog progressDialog;
+      private Exception exception = null;
+
+      RotateTask(Page page, RotationAngle rotationAngle) {
+         this.rotationAngle = rotationAngle;
+         this.page = page;
+      }
+
+      @Override
+      protected void onPreExecute() {
+         super.onPreExecute();
+         progressDialog = new ProgressDialog(ScanActivity.this);
+         progressDialog.show();
+      }
+
+      @Override
+      protected Void doInBackground(Void... params) {
+         String path = page.getOriginalImage().getAbsolutePath(ScanActivity.this);
+         if (rotationAngle != RotationAngle.ROTATION_0) {
+            try {
+               GeniusScanSDK.rotateImage(path, path, rotationAngle);
+            } catch (Exception e) {
+               exception = e;
+            }
+         }
+
+         return null;
+      }
+
+      @Override
+      protected void onPostExecute(Void aVoid) {
+         super.onPostExecute(aVoid);
+         progressDialog.dismiss();
+
+         if (exception instanceof LicenseException) {
+            new AlertDialog.Builder(ScanActivity.this)
+                    .setMessage(exception.getMessage())
+                    .setPositiveButton(android.R.string.ok, null)
+                    .show();
+         } else if (exception != null) {
+            throw new RuntimeException(exception);
+         } else {
+            Intent intent = new Intent(ScanActivity.this, BorderDetectionActivity.class);
+            intent.putExtra(BorderDetectionActivity.EXTRA_PAGE, page);
+            startActivity(intent);
+         }
+      }
+   }
+
+}
---
+++ android/demo-custom/src/main/java/com/geniusscansdk/demo/processing/BorderDetectionActivity.java
@@ -0,0 +1,90 @@
+package com.geniusscansdk.demo.processing;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import com.geniusscansdk.demo.R;
+import com.geniusscansdk.demo.enhance.ImageProcessingActivity;
+import com.geniusscansdk.demo.model.Page;
+import com.geniusscansdk.scanflow.BorderDetectionImageView;
+import com.geniusscansdk.scanflow.MagnifierBorderDetectionListener;
+import com.geniusscansdk.scanflow.MagnifierView;
+
+public class BorderDetectionActivity extends AppCompatActivity {
+
+   @SuppressWarnings("unused")
+   private static final String TAG = BorderDetectionActivity.class.getSimpleName();
+
+   private Page page;
+
+   private ProgressDialog progressDialog;
+   private BorderDetectionImageView imageView;
+   private MagnifierView magnifierView;
+
+   public final static String EXTRA_PAGE = "page";
+
+   @Override
+   protected void onCreate(Bundle savedInstanceState) {
+      super.onCreate(savedInstanceState);
+
+      setContentView(R.layout.border_detection_activity);
+
+      imageView = findViewById(R.id.image_view);
+      imageView.setOverlayColorResource(R.color.blue);
+
+      magnifierView = findViewById(R.id.magnifier_view);
+      imageView.setListener(new MagnifierBorderDetectionListener(magnifierView));
+
+      page = getIntent().getParcelableExtra(EXTRA_PAGE);
+   }
+
+   @Override
+   protected void onResume() {
+      super.onResume();
+      String filename = page.getOriginalImage().getAbsolutePath(this);
+      BitmapFactory.Options opts = new BitmapFactory.Options();
+      opts.inSampleSize = 2;
+
+      Bitmap bitmap = BitmapFactory.decodeFile(filename, opts);
+      imageView.setImageBitmap(bitmap);
+      magnifierView.setBitmap(bitmap);
+
+      progressDialog = new ProgressDialog(this);
+
+      progressDialog.show();
+      new AnalyzeAsyncTask(this) {
+         @Override
+         protected void onPostExecute(Void aVoid) {
+            super.onPostExecute(aVoid);
+            endAnalyze();
+         }
+      }.execute(page);
+   }
+
+   protected void endAnalyze() {
+      progressDialog.dismiss();
+      addQuadrangleToView();
+   }
+
+   void addQuadrangleToView() {
+      imageView.setQuad(page.getQuadrangle());
+      imageView.invalidate();
+   }
+
+   public void setQuadrangleToFullImage(View view) {
+      page.getQuadrangle().setToFullImage();
+      imageView.invalidate();
+   }
+
+   public void select(View view) {
+      Intent intent = new Intent(this, ImageProcessingActivity.class);
+      intent.putExtra(ImageProcessingActivity.EXTRA_PAGE, page);
+      startActivity(intent);
+   }
+
+}
