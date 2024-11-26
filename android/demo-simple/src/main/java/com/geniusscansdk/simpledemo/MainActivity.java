b6e78183671c000dfd70e7a3a5bd00f6c52d2995
The Grizzly Labs <sdk@thegrizzlylabs.com>
2024-11-26 11:44:27 +0000

    Update demos for version 5.4.0

--- android/demo-simple/src/main/java/com/geniusscansdk/simpledemo/MainActivity.java
+++ android/demo-simple/src/main/java/com/geniusscansdk/simpledemo/MainActivity.java
@@ -1,28 +1,41 @@
 package com.geniusscansdk.simpledemo;
 
-import static com.geniusscansdk.scanflow.ScanConfiguration.Filter.*;
+import static com.geniusscansdk.scanflow.ScanConfiguration.Filter.AUTOMATIC;
+import static com.geniusscansdk.scanflow.ScanConfiguration.Filter.AUTOMATIC_BLACK_AND_WHITE;
+import static com.geniusscansdk.scanflow.ScanConfiguration.Filter.AUTOMATIC_COLOR;
+import static com.geniusscansdk.scanflow.ScanConfiguration.Filter.NONE;
+import static com.geniusscansdk.scanflow.ScanConfiguration.Filter.PHOTO;
+import static com.geniusscansdk.scanflow.ScanConfiguration.Filter.SOFT_COLOR;
+import static com.geniusscansdk.scanflow.ScanConfiguration.Filter.SOFT_GRAYSCALE;
+import static com.geniusscansdk.scanflow.ScanConfiguration.Filter.STRONG_MONOCHROME;
 
 import android.app.Activity;
-import android.app.AlertDialog;
 import android.content.Intent;
 import android.graphics.Color;
 import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.annotation.RawRes;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.content.ContextCompat;
 import androidx.core.content.FileProvider;
+import androidx.core.graphics.Insets;
+import androidx.core.view.OnApplyWindowInsetsListener;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
 
 import com.geniusscansdk.core.LicenseException;
 import com.geniusscansdk.scanflow.ScanConfiguration;
 import com.geniusscansdk.scanflow.ScanFlow;
 import com.geniusscansdk.scanflow.ScanResult;
+import com.google.android.material.dialog.MaterialAlertDialogBuilder;
 
 import java.io.File;
 import java.io.FileOutputStream;
@@ -38,10 +51,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
+
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setTitle("GS SDK Simple Demo");
+        ViewCompat.setOnApplyWindowInsetsListener(toolbar, new OnApplyWindowInsetsListener() {
+            @NonNull
+            @Override
+            public WindowInsetsCompat onApplyWindowInsets(@NonNull View v, @NonNull WindowInsetsCompat insets) {
+                Insets systemInsets = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+                v.setPadding(v.getPaddingLeft(), systemInsets.top, v.getPaddingRight(), v.getPaddingBottom());
+                return insets;
+            }
+        });
 
         // This code shows how to initialize the SDK with a license key.
         // Without a license key, the SDK runs for 60 seconds and then the app needs to be restarted.
@@ -67,9 +90,9 @@ public class MainActivity extends AppCompatActivity {
         scanConfiguration.photoLibraryButtonHidden = false;
         scanConfiguration.flashButtonHidden = false;
         scanConfiguration.defaultFlashMode = ScanConfiguration.FlashMode.AUTO;
-        scanConfiguration.backgroundColor = Color.WHITE;
-        scanConfiguration.foregroundColor = ContextCompat.getColor(this, R.color.colorPrimary);
-        scanConfiguration.highlightColor = ContextCompat.getColor(this, R.color.colorAccent);
+        scanConfiguration.backgroundColor = ContextCompat.getColor(this, R.color.md_theme_background);
+        scanConfiguration.foregroundColor = ContextCompat.getColor(this, R.color.md_theme_primary);
+        scanConfiguration.highlightColor = Color.BLUE;
         scanConfiguration.availableFilters = Arrays.asList(NONE, AUTOMATIC, AUTOMATIC_BLACK_AND_WHITE,
                 AUTOMATIC_COLOR, PHOTO, SOFT_GRAYSCALE, SOFT_COLOR, STRONG_MONOCHROME
         );
@@ -137,20 +160,20 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             } catch (LicenseException e) {
                 if (e.errorCode == LicenseException.ErrorCode.ExpiredDemo) {
-                    new AlertDialog.Builder(this)
+                    new MaterialAlertDialogBuilder(this)
                             .setMessage(e.getMessage())
                             .setPositiveButton("Restart", (dialog, which) -> restartApp())
                             .show();
                 } else {
                     // The license key is invalid or expired, either ask the user to update the app or provide a fallback
-                    new AlertDialog.Builder(this)
+                    new MaterialAlertDialogBuilder(this)
                             .setMessage("Please update to the latest version.")
                             .setPositiveButton(android.R.string.ok, (dialogInterface, i) -> {})
                             .show();
                 }
             } catch (Exception e) {
                 Log.e(TAG, "Error during scan flow", e);
-                new AlertDialog.Builder(this)
+                new MaterialAlertDialogBuilder(this)
                         .setMessage("An error occurred: " + e.getMessage())
                         .setPositiveButton(android.R.string.ok, (dialogInterface, i) -> {})
                         .show();
